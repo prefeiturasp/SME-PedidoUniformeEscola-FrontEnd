9ac085cd4c433a1f824d75d54e6f95d948a04b40
Calvin Rossinhole <calvin.masters@gmail.com>
2020-02-25 16:38:23 -0300

    pagina lista-alunos; faz requicao da busca

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,9 +1,15 @@
 import DashboardPage from "../pages/DashboardPage";
+import ListaAlunosPage from "../pages/ListaAlunosPage";
 
 export const routes = [
   {
     path: "/",
     component: DashboardPage,
+    exact: true
+  },
+  {
+    path: "/lista-alunos",
+    component: ListaAlunosPage,
     exact: true
   }
 ];
---
+++ src/services/listaAlunos.service.js
@@ -0,0 +1,40 @@
+import { API_URL } from "../config";
+import { AUTH_TOKEN } from "./constants";
+
+export const getListaAlunos = getParams => {
+  const url = `${API_URL}/alunos/${getParams}`;
+  let status = 0;
+  return fetch(url, {
+    method: "GET",
+    headers: AUTH_TOKEN
+  })
+    .then(res => {
+      status = res.status;
+      return res.json();
+    })
+    .then(data => {
+      return { data: data, status: status };
+    })
+    .catch(error => {
+      return error.json();
+    });
+};
+
+export const getTodosAlunos = () => {
+  const url = `${API_URL}/alunos/`;
+  let status = 0;
+  return fetch(url, {
+    method: "GET",
+    headers: AUTH_TOKEN
+  })
+    .then(res => {
+      status = res.status;
+      return res.json();
+    })
+    .then(data => {
+      return { data: data, status: status };
+    })
+    .catch(error => {
+      return error.json();
+    });
+};
