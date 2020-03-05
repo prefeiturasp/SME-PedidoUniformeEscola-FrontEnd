c6a56e2820ff7a2fda8d824965938f0c78b4577d
Calvin Rossinhole <calvin.masters@gmail.com>
2020-03-05 11:21:54 -0300

    ultimos ajustes

--- src/screens/Dashboard/index.jsx
+++ src/screens/Dashboard/index.jsx
@@ -5,7 +5,9 @@ export default class Dashboard extends Component {
     return (
       <div className="card">
         <div className="card-body">
-          Aqui será o Dashboard
+          Em breve esta página apresentará o painel gerencial, com os
+          quantitativos de quantas famílias já realizaram a solicitação do
+          uniforme e o status delas. Aguarde!
         </div>
       </div>
     );
