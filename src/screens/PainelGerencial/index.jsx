06568602201ba75c361fd5026afcf4490280352e
Calvin Rossinhole <calvin.masters@gmail.com>
2020-04-08 17:34:52 -0300

    painel visao dre - inicio

--- src/screens/PainelGerencial/index.jsx
+++ src/screens/PainelGerencial/index.jsx
@@ -6,18 +6,20 @@ import GraficoPizza from "../../components/GraficoPizza";
 import "./style.scss";
 import { formatarDados } from "./helper";
 import { perfilEscola } from "../../helpers/utils";
+import { FiltroEscolas } from "./componentes/FiltroEscolas";
 
 export class PainelGerencial extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      openCollapse: false,
       dados: null,
-      responseError: false
+      responseError: false,
     };
   }
 
   componentDidMount() {
-    getDadosPainelGerencial().then(response => {
+    getDadosPainelGerencial().then((response) => {
       if (response.status === HTTP_STATUS.OK) {
         this.setState({ dados: response.data.results });
       } else {
@@ -26,13 +28,25 @@ export class PainelGerencial extends Component {
     });
   }
 
+  alterCollapse = () => {
+    this.setState({ openCollapse: !this.state.openCollapse });
+  };
+
+  closeCollapse = () => {
+    this.setState({ openCollapse: false });
+  };
+
   render() {
-    const { dados, responseError } = this.state;
+    const { dados, responseError, openCollapse } = this.state;
     return (
       <div className="panel-management">
         <Fragment>
           <div className="card">
             <div className="card-body">
+              <FiltroEscolas
+                openCollapse={openCollapse}
+                alterCollapse={this.alterCollapse}
+              />
               {!dados && !responseError && <div>Carregando... </div>}
               {responseError && <div>Erro ao carregar painel gerencial.</div>}
               {dados && (
