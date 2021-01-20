0512d00f5ba6dc6bddcc655482284313608b3e5e
Kelwy <kelwyduarte@gmail.com>
2021-01-20 08:59:57 -0300

    Coloca site no ar

---
+++ src/screens/Login/index_site_fora_do_ar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Login = () => {
+  return (
+    <div className="row pt-5">
+      <div className="col-sm-6 col-10 offset-sm-3 offset-1">
+        <div className="card mb-5">
+          <div className="card-body">
+            No Programa Auxilio Uniforme Escolar implantado neste ano (2021), 
+            as famílias não precisam mais fazer a solicitação do crédito. 
+            Todos os estudantes matriculados na Rede Municipal de Ensino 
+            até 1º de abril receberão o valor de R$ 387,10 para comprar o 
+            uniforme escolar nas lojas credenciadas, por meio da Mercado Pago. 
+            <br />
+            <br />
+              As atualizações cadastrais de dados dos responsáveis, portanto, 
+              devem voltar a ser feitas diretamente no Sistema Escola Online (EOL).
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
